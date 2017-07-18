1a03a2fea5ba4160fdd7000a46e7b05e87cc97ed
Victor Hernandez <vikhhr@gmail.com>
2017-07-17 21:45:50 -0600

    Adding Service Worker.

--- 1-12-skeleton/scripts/app.js
+++ 1-12-skeleton/scripts/app.js
@@ -198,4 +198,12 @@
       }
     });
   });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(function() {
+        console.log('Service Worker Registered');
+      });
+  }
 })();
